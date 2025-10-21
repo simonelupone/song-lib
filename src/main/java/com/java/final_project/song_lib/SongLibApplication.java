e53f0a720529d3a9f0363fe75631de780f559dae
simonelupone <simone.lupone@gmail.com>
2025-10-21 19:47:37 +0200

    first commit

---
+++ src/main/java/com/java/final_project/song_lib/SongLibApplication.java
@@ -0,0 +1,13 @@
+package com.java.final_project.song_lib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongLibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/final_project/song_lib/SongLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.final_project.song_lib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
